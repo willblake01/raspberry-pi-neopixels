0e4eb3599801b37ce289d95dbec4412fb5848f76
William Blake <willblakebooking@gmail.com>
2025-09-27 21:56:25 -0500

    Refactor change

--- modes/change.js
+++ modes/change.js
@@ -5,21 +5,20 @@ export class Change {
   constructor(config, interval) {
     this.config = config;
     this.interval = interval;
+    this.pixels = new Uint32Array(this.config.leds);
 
     ws281x.configure(config);
   };
 
   loop() {
-    const pixels = new Uint32Array(this.config.leds);
-
     const red = randomNumber(255), green = randomNumber(255), blue = randomNumber(255);
     const color = (red << 16) | (green << 8) | blue;
 
     for (let i = 0; i < this.config.leds; i++) {
-      pixels[i] = color;
+      this.pixels[i] = color;
     };
 
-    ws281x.render(pixels);
+    ws281x.render(this.pixels);
   };
 
   run() {
