d48a4739108b1bd68ad7b25464c10d1ffc25475d
William Blake <willblakebooking@gmail.com>
2025-11-07 15:15:53 -0600

    Refactor turnOff

--- modes/turnOff.js
+++ modes/turnOff.js
@@ -8,18 +8,13 @@ export class TurnOff {
     };
 
     run() {
-      const pixels = new Uint32Array(this.config.leds);
-
-      ws281x.render(pixels);
-
       process.on('SIGINT', () => {
         ws281x.reset();
+        ws281x.finalize();
+
         process.nextTick(() => {
           process.exit(0);
         });
       });
-
-      ws281x.reset();
-      process.exit(0);
     };
   };
