5330e335c71be0492a5328c054cd3b3bce361a7c
William Blake <willblakebooking@gmail.com>
2025-11-07 16:50:23 -0600

    Add Effect Manager

---
+++ EffectManager.js
@@ -0,0 +1,28 @@
+import ws281x from 'rpi-ws281x';
+
+export class EffectManager {
+  constructor(config) {
+    this.config = config;
+    this.current = null;
+
+    ws281x.configure(config);
+  }
+
+  start(effect) {
+    this.stop();
+    this.current = effect;
+    effect.run(config);
+  }
+
+  stop() {
+    if (this.current?.stop) {
+      try { this.current.stop(); } catch {}
+      this.current = null;
+    }
+  }
+
+  dispose() {
+    this.stop();
+    try { ws281x.reset(); } catch {}
+  }
+}
