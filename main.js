e7243702e2dba240ae93c139296e6e2b41795c5a
William Blake <willblakebooking@gmail.com>
2025-11-09 16:52:29 -0600

    Modularize prompts

--- main.js
+++ main.js
@@ -3,48 +3,13 @@ import { EffectManager } from './EffectManager.js';
 import { questions } from './prompts/questions.js';
 import { TurnOff } from './modes/index.js';
 import { RULES } from './utils/index.js';
+import { normalizeAnswers } from './prompts/normalizeAnswers.js'; 
 
 const delay = (ms) => new Promise(r => setTimeout(r, ms));
 
 const once = (fn) => {
   let called = false;
   return (...args) => { if (called) return; called = true; return fn(...args); };
-};
-
-const normalizeAnswers = (res) => {
-  const {blue, brightness, colorMode, colorChangeInterval, command, green, interval, mode, leds, pixelState, randomColorMode, red} = res;
-
-  return {
-    leds,
-    brightness,
-    interval,
-    red,
-    green,
-    blue,
-    command,
-    mode,
-    colorMode,
-    randomColorMode,
-    colorChangeInterval,
-    pixelState,
-    isOn: command === 1,
-    isOff: command === 0,
-    isSolid: mode === 'solid',
-    isChange: mode === 'change',
-    isBlink: mode === 'blink',
-    isBreathe: mode === 'breathe',
-    isCreep: mode === 'creep',
-    isWheel: mode === 'wheel',
-    isWalkPixel: mode === 'walk pixel',
-    isCustom: colorMode === 'custom',
-    isRandom: colorMode === 'random',
-    isStaticRC: randomColorMode === 'static',
-    isChangeRC: randomColorMode === 'change',
-    everyPixel: colorChangeInterval === 'everyPixel',
-    everyLoop: colorChangeInterval === 'everyLoop',
-    pixelOn: pixelState === 1,
-    pixelOff: pixelState === 0 
-  };
 };
 
 const selectEffect = (config, opts) => {
