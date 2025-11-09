3e39c30839f1b7e5b22b5caad51bf6403cc49b1f
William Blake <willblakebooking@gmail.com>
2025-11-09 13:51:45 -0600

    Refactor if statements in entry point

--- neopixels.js
+++ neopixels.js
@@ -3,173 +3,214 @@ import { EffectManager } from './EffectManager.js';
 import { questions } from './prompts/questions.js';
 import { BlinkCustomColor, BlinkRandomColorChange, BlinkRandomColorStatic, BreatheCustomColor, Change, CreepCustomColor, CreepRandomColorChangePixel, CreepRandomColorChangeStrand, CreepRandomColorStatic, SolidCustomColor, SolidRandomColor, WalkOffPixelCustomColor, WalkOffPixelRandomColorPixel, WalkOffPixelRandomColorStrand, WalkPixelCustomColor, WalkPixelRandomColorPixel, WalkPixelRandomColorStrand, Wheel, TurnOff } from './modes/index.js';
 
-(async () => {
-  const response = await prompts(questions);
+const delay = (ms) => new Promise(r => setTimeout(r, ms));
 
-  const { blue, brightness, colorMode, colorChangeInterval, command, green, interval, mode, leds, pixelState, randomColorMode, red } = response;
+const once = () => {
+  let called = false;
+  return (...args) => { if (called) return; called = true; return fn(...args); };
+};
+
+const normalizeAnsers = (res) => {
+  const {blue, brightness, colorMode, colorChangeInterval, command, green, interval, mode, leds, pixelState, randomColorMode, red} = res;
+
+  return {
+    leds,
+    brightness,
+    interval,
+    red,
+    green,
+    blue,
+    command,
+    mode,
+    colorMode,
+    randomColorMode,
+    colorChangeInterval,
+    pixelState,
+    isOn: command === 1,
+    isOff: command === 0,
+    isSolid: mode === 'solid',
+    isChange: mode === 'change',
+    isBlink: mode === 'blink',
+    isBreathe: mode === 'breathe',
+    isCreep: mode === 'creep',
+    isWheel: mode === 'wheel',
+    isWalkPixel: mode === 'walk pixel',
+    isCustom: colorMode === 'custom',
+    isRandom: colorMode === 'random',
+    isStaticRC: randomColorMode === 'static',
+    isChangeRC: randomColorMode === 'change',
+    everyPixel: colorChangeInterval === 'everyPixel',
+    everyLoop: colorChangeInterval === 'everyLoop',
+    pixelOn: pixelState === 1,
+    pixelOff: pixelState === 0 
+  };
+};
+
+const RULES = [
+  // --- SOLID ---
+  {
+    name: 'solid-custom',
+    when: o.isSolid && o.isCustom,
+    make: (cfg, o) => new SolidCustomColor(cfg, o.red, o.green, o.blue),
+  },
+  {
+    name: 'solid-random',
+    when: o => o.isSolid && o.isRandom,
+    make: (cfg) => new SolidRandomColor(cfg),
+  },
+
+  // --- CHANGE ---
+  {
+    name: 'change',
+    when: o => o.isChange,
+    make: (cfg, o) => new Change(cfg, o.interval),
+  },
+
+  // --- BLINK ---
+  {
+    name: 'blink-custom',
+    when: o => o.isBlink && o.isCustom,
+    make: (cfg, o) => new BlinkCustomColor(cfg, o.interval, o.red, o.green, o.blue),
+  },
+  {
+    name: 'blink-random-static',
+    when: o => o.isBlink && o.isRandom && o.isStaticRC,
+    make: (cfg, o) => new BlinkRandomColorStatic(cfg, o.interval),
+  },
+  {
+    name: 'blink-random-change',
+    when: o => o.isBlink && o.isRandom && o.isChangeRC,
+    make: (cfg, o) => new BlinkRandomColorChange(cfg, o.interval),
+  },
+
+  // --- BREATHE ---
+  {
+    name: 'breathe-custom',
+    when: o => o.isBreathe && o.isCustom,
+    make: (cfg, o) => new BreatheCustomColor(cfg, o.red, o.green, o.blue, o.interval),
+  },
+
+  // --- CREEP ---
+  {
+    name: 'creep-custom',
+    when: o => o.isCreep && o.isCustom,
+    make: (cfg, o) => new CreepCustomColor(cfg, o.interval, o.red, o.green, o.blue),
+  },
+  {
+    name: 'creep-random-static',
+    when: o => o.isCreep && o.isRandom && o.isStaticRC,
+    make: (cfg, o) => new CreepRandomColorStatic(cfg, o.interval),
+  },
+  {
+    name: 'creep-random-change-everyPixel',
+    when: o => o.isCreep && o.isRandom && o.isChangeRC && o.everyPixel,
+    make: (cfg, o) => new CreepRandomColorChangePixel(cfg, o.interval),
+  },
+  {
+    name: 'creep-random-change-everyLoop',
+    when: o => o.isCreep && o.isRandom && o.isChangeRC && o.everyLoop,
+    make: (cfg, o) => new CreepRandomColorChangeStrand(cfg, o.interval),
+  },
+
+  // --- WHEEL ---
+  {
+    name: 'wheel',
+    when: o => o.isWheel,
+    make: (cfg, o) => new Wheel(cfg, o.interval),
+  },
+
+  // --- WALK PIXEL (pixel ON) ---
+  {
+    name: 'walkPixel-on-custom',
+    when: o => o.isWalkPixel && o.pixelOn && o.isCustom,
+    make: (cfg, o) => new WalkPixelCustomColor(cfg, o.interval, o.red, o.green, o.blue),
+  },
+  {
+    name: 'walkPixel-on-random-everyPixel',
+    when: o => o.isWalkPixel && o.pixelOn && o.isRandom && o.everyPixel,
+    make: (cfg, o) => new WalkPixelRandomColorPixel(cfg, o.interval),
+  },
+  {
+    name: 'walkPixel-on-random-everyLoop',
+    when: o => o.isWalkPixel && o.pixelOn && o.isRandom && o.everyLoop,
+    make: (cfg, o) => new WalkPixelRandomColorStrand(cfg, o.interval),
+  },
+
+  // --- WALK PIXEL (pixel OFF) ---
+  {
+    name: 'walkPixel-off-custom',
+    when: o => o.isWalkPixel && o.pixelOff && o.isCustom,
+    make: (cfg, o) => new WalkOffPixelCustomColor(cfg, o.interval, o.red, o.green, o.blue),
+  },
+  {
+    name: 'walkPixel-off-random-everyPixel',
+    when: o => o.isWalkPixel && o.pixelOff && o.isRandom && o.everyPixel,
+    make: (cfg, o) => new WalkOffPixelRandomColorPixel(cfg, o.interval),
+  },
+  {
+    name: 'walkPixel-off-random-everyLoop',
+    when: o => o.isWalkPixel && o.pixelOff && o.isRandom && o.everyLoop,
+    make: (cfg, o) => new WalkOffPixelRandomColorStrand(cfg, o.interval),
+  },
+
+  // --- TURN OFF (explicit command) ---
+  {
+    name: 'turnOff',
+    when: o => o.isOff,
+    make: (cfg) => new TurnOff(cfg),
+  },
+];
+
+const selectEffect = (config, opts) => {
+  if (opts.isOff) return new TurnOff(config);
+  const rule = RULES.find(r => r.when(opts));
+  if (!rule) return new TurnOff(config);
+  return rule.make(config, opts);
+};
+
+const main = async () => {
+  const response = await prompts(questions);
+  const o = normalizeAnsers(response);
 
   const config = {
-    leds: leds,
+    leds:o.leds,
     dma: 10,
-    brightness: brightness,
+    brightness: o.brightness,
     gpio: 18,
     stripType: 'rgb'
   };
 
-  // Set delay
-  const timeout = 1000;
-
-  const turnOn = command === 1;
-  const turnOff = command === 0;
-  const solidMode = mode === 'solid';
-  const changeMode = mode === 'change';
-  const blinkMode = mode === 'blink';
-  const breatheMode = mode === 'breathe';
-  const creepMode = mode === 'creep';
-  const wheelMode = mode === 'wheel';
-  const walkPixelMode = mode === 'walk pixel';
-  const staticRandomColorMode = randomColorMode === 'static';
-  const changeRandomColorMode = randomColorMode === 'change';
-  const customColor = colorMode === 'custom';
-  const randomColor = colorMode === 'random';
-  const everyPixelChangeInterval = colorChangeInterval === 'everyPixel';
-  const everyLoopChangeInterval = colorChangeInterval === 'everyLoop'
-  const pixelOn = pixelState === 1;
-  const pixelOff = pixelState === 0;
-
   const manager = new EffectManager(config);
-
-  const handleMode = () => {
-    // Solid Mode
-    if (solidMode && customColor) {
-      const solidCustomColor = new SolidCustomColor(manager.config, red, green, blue);
-      manager.start(solidCustomColor);
-    };
-
-    if (solidMode && randomColor) {
-      const solidRandomColor = new SolidRandomColor(manager.config);
-      manager.start(solidRandomColor);
-    };
-
-    // Change Mode
-    if (changeMode) {
-      const change = new Change(manager.config, interval);
-      manager.start(change);
-    };
-
-    // Blink Mode
-    if (blinkMode && customColor) {
-      const blinkCustomColor = new BlinkCustomColor(manager.config, interval, red, green, blue);
-      manager.start(blinkCustomColor);
-    };
-
-    if (blinkMode && randomColor && staticRandomColorMode) {
-      const blinkRandomColorStatic = new BlinkRandomColorStatic(manager.config, interval);
-      manager.start(blinkRandomColorStatic);
-    };
-
-    if (blinkMode && randomColor && changeRandomColorMode) {
-      const blinkRandomColorChange = new BlinkRandomColorChange(manager.config, interval);
-      manager.start(blinkRandomColorChange);
-    };
-
-    // Breathe Mode
-    if (breatheMode && customColor) {
-      const breatheCustomColor = new BreatheCustomColor(manager.config, red, green, blue, interval);
-      manager.start(breatheCustomColor);
-    };
-
-    // Creep Mode
-    if (creepMode && customColor) {
-      const creepCustomColor = new CreepCustomColor(manager.config, interval, red, green, blue);
-      manager.start(creepCustomColor);
-    };
-
-    if (creepMode && randomColor && staticRandomColorMode) {
-      const creepRandomColorStatic = new CreepRandomColorStatic(manager.config, interval);
-      manager.start(creepRandomColorStatic);
-    };
-
-    if (creepMode && randomColor && changeRandomColorMode && everyPixelChangeInterval) {
-      const creepRandomColorChangePixel = new CreepRandomColorChangePixel(manager.config, interval);
-      manager.start(creepRandomColorChangePixel);
-    };
-
-    if (creepMode && randomColor && changeRandomColorMode && everyLoopChangeInterval) {
-      const creepRandomColorChangeStrand = new CreepRandomColorChangeStrand(manager.config, interval);
-      manager.start(creepRandomColorChangeStrand);
-    };
-
-    // Wheel Mode
-    if (wheelMode) {
-      const wheel = new Wheel(manager.config, interval);
-      manager.start(wheel);
-    };
-
-    // Walk Pixel Mode
-    if (walkPixelMode && pixelOn && customColor) {
-      const walkPixelCustomColor = new WalkPixelCustomColor(manager.config, interval, red, green, blue);
-      manager.start(walkPixelCustomColor);
-    };
-
-    if (walkPixelMode && pixelOn && randomColor && everyPixelChangeInterval) {
-      const walkPixelRandomColorPixel = new WalkPixelRandomColorPixel(manager.config, interval);
-      manager.start(walkPixelRandomColorPixel);
-    };
-
-    if (walkPixelMode && pixelOn && randomColor && everyLoopChangeInterval) {
-      const walkPixelRandomColorStrand = new WalkPixelRandomColorStrand(manager.config, interval);
-      manager.start(walkPixelRandomColorStrand);
-    };
-
-    if (walkPixelMode && pixelOff && customColor) {
-      const walkOffPixelCustomColor = new WalkOffPixelCustomColor(manager.config, interval, red, green, blue);
-      manager.start(walkOffPixelCustomColor);
-    };
-
-    if (walkPixelMode && pixelOff && randomColor && everyPixelChangeInterval) {
-      const walkOffPixelRandomColorPixel = new WalkOffPixelRandomColorPixel(manager.config, interval);
-      manager.start(walkOffPixelRandomColorPixel);
-    };
-
-    if (walkPixelMode && pixelOff && randomColor && everyLoopChangeInterval) {
-      const walkOffPixelRandomColorStrand = new WalkOffPixelRandomColorStrand(manager.config, interval);
-      manager.start(walkOffPixelRandomColorStrand);
-    };
-  };
-
-  const handleCommand = () => {
-    if (turnOn) {
-      handleMode();
-    };
-
-    if (turnOff) {
-      const turnOff = new TurnOff(manager.config);
-      manager.start(turnOff);
-    };
-  };
 
   // Graceful exit hooks
   let shuttingDown = false;
 
-  const shutDown = (reason, err) => {
+  const shutDown = once((reason, err) => {
     if (shuttingDown) return;
     shuttingDown = true;
     try { 
       manager.dispose();
     } finally {
-    if (err) { 
-      console.error(`[shutdown ${reason}]`, err);
-      process.exitCode = 1;
-    }
+      if (err) { 
+        console.error(`[shutdown ${reason}]`, err);
+        process.exitCode = 1;
+      }
       setTimeout(() => process.exit(), 10);
     }
-  };
+  });
 
   process.once('SIGINT', () => shutDown('SIGINT'));
   process.once('SIGTERM', () => shutDown('SIGTERM'));
   process.once('uncaughtException', (err) => shutDown('uncaughtException', err));
   process.once('unhandledRejection', (err) => shutDown('unhandledRejection', err));
 
-  setTimeout(handleCommand, timeout);
-})();
+  await delay(1000);
+
+  const effect = selectEffect(manager.config, o);
+  manager.start(effect);
+};
+
+main().catch(err => {
+  console.error('[fatal]', err);
+  process.exitCode = 1;
+});
