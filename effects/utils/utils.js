07f54f28735f8428cea1c89a103d75eef9920850
William Blake <gwilliamblake@gmail.com>
2025-11-10 15:16:01 -0600

    Fix setPixelColor

--- effects/utils/utils.js
+++ effects/utils/utils.js
@@ -2,15 +2,15 @@ export const setPixelColor = (pixelCount, offset, color1, color2) => {
   const pixels = new Uint32Array(pixelCount);
 
     if (offset !== undefined) {
-      for (let i = 0; i < this.offset; i++) {
+      for (let i = 0; i < offset; i++) {
         pixels[i] = color1;
       };
 
-      for (let i = this.offset; i < this.config.leds; i++) {
+      for (let i = offset; i < pixelCount; i++) {
         pixels[i] = color2;
       };
 
-      pixels[this.offset] = this.color1;
+      pixels[offset] = color1;
     } else {
       for (let i = 0; i < pixelCount; i++) {
         pixels[i] = color1;
