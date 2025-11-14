29403b7b192ec49bc7ae0cac9099f403cce7cc36
William Blake <willblakebooking@gmail.com>
2025-11-13 22:50:38 -0600

    ...

--- effects/utils/routingTable.js
+++ effects/utils/routingTable.js
@@ -24,7 +24,7 @@ export const RULES = [
   {
     name: 'blink-custom',
     when: options => options.isBlink && options.isCustomColorMode,
-    make: (config, options) => new BlinkCustom(config, options.interval, options.red, options.green, options.blue),
+    make: (config, options) => new BlinkCustom(config, ...options),
   },
   {
     name: 'blink-random-static',
