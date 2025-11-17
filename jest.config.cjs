90e3ab9237497f76a1decc13a7c574f53dcf0353
William Blake <willblakebooking@gmail.com>
2025-11-17 16:39:24 -0600

    ...

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,23 +1,24 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.test.json"
-    }
-  },
-
-  // Map "./foo.js" import in TS to "./foo" so Jest finds foo.ts
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1"
-  },
-
+  // Tell ts-jest to use CommonJS for Jest, even though your app uses ESM
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json'
+      }
+    ]
   },
 
-  // Only run tests from src/, ignore built JS in dist
-  testMatch: ["**/src/**/*.test.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
+  // Allow TS files to import "./file.js" but map to "./file.ts"
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+
+  // Only match test files inside src/
+  testMatch: ['**/src/**/*.test.ts'],
+
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
 };
