8d56a76f77747ec5c5a16485f96af772020ee122
William Blake <willblakebooking@gmail.com>
2025-11-17 15:28:22 -0600

    ...

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
+  // TS + ESM via ts-jest
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
 
+  // Treat .ts as ESM
   extensionsToTreatAsEsm: [".ts"],
 
   globals: {
@@ -11,14 +13,16 @@ module.exports = {
     },
   },
 
+  // Map "./foo.js" in TS to "./foo" so Jest loads foo.ts
   moduleNameMapper: {
-    // Take './foo.js' in TS and resolve './foo' (i.e., foo.ts) for Jest
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
 
+  // Make sure ALL TS files go through ts-jest
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
   },
 
+  // Don’t accidentally run compiled JS tests
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
